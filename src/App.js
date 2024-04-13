df03b822c059a923b9233bc87b634e6d01a86238
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-13 22:35:05 +0200

    Added Options.js component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Options from "./Options";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>What should you watch tonight?</h1>
+        <Options />
+      </div>
+    </div>
+  );
 }
 
 export default App;
